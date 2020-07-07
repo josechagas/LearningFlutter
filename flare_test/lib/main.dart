92a7dd25b17eec2fb84466ebf652e9fe4e94ed01
José Lucas Souza das Chagas <joselucas@Joses-MacBook-Pro.local>
2020-07-07 07:31:48 -0300

    using Flutter with Flare for animations

---
+++ flare_test/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: FlareActor(
+            'assets/Pulsing Heart.flr2d',
+          ),
+        ),
+      ),
+    );
+  }
+}
