bc367759527c9a5e06e73dd35195b3b9df36c5f3
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2020-08-11 08:23:34 -0300

    finishing login animation

--- flutter_animations/lib/ui/login_page.dart
+++ flutter_animations/lib/ui/login_page.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animations/ui/widgets/form_container.dart';
+import 'package:flutter_animations/ui/widgets/stagger_animation.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage>  with SingleTickerProviderStateMixin {
+
+  AnimationController _animController;
+
+  @override
+  void initState() {
+    _animController =  AnimationController(
+        vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    timeDilation = 1;
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
+          overflow: Overflow.visible,
+          alignment: Alignment(0,0.3),
           children: <Widget>[
             Image.asset(
               'images/background.jpg',
@@ -21,20 +43,26 @@ class _LoginPageState extends State<LoginPage> {
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 200, bottom: 10),
-                  child: Image.asset(
+            Positioned(
+              top: 80,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
                     'images/tickicon.png',
                     fit: BoxFit.contain,
                     height: 150,
                   ),
-                ),
-                FormContainer(),
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FormContainer(),
+                ],
+              ),
             ),
             Positioned(
               left: 40,
@@ -47,7 +75,10 @@ class _LoginPageState extends State<LoginPage> {
                 textColor: Colors.white,
                 onPressed: (){},
               ),
-            )
+            ),
+            StaggerAnimation(
+              controller: _animController,
+            ),
           ],
         ),
       ),
---
+++ flutter_animations/lib/ui/widgets/stagger_animation.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class StaggerAnimation extends StatelessWidget {
+  StaggerAnimation({this.controller, Key key})
+      : buttonSqueeze = Tween(begin: 320.0, end: 60.0).animate(
+          CurvedAnimation(
+              parent: controller,
+              curve: Interval(
+                  0, 0.150) //define quando comeca e quando termina em %
+              ),
+        ),
+        buttonZoomOut = Tween(begin: 0.0, end:1.0).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.5, 1, curve: Curves.fastLinearToSlowEaseIn),
+          ),
+        ),
+        super(key: key);
+
+  final AnimationController controller;
+  final Animation<double> buttonSqueeze;
+  final Animation<double> buttonZoomOut;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: controller,
+      builder: _buildAnimation,
+    );
+  }
+
+  Widget _buildAnimation(BuildContext context, Widget child) {
+    final zoomOutHeight = buttonZoomOut.value*MediaQuery.of(context).size.height;
+    final zoomOutWidth = buttonZoomOut.value*MediaQuery.of(context).size.width;
+    final zoomSide = zoomOutWidth < zoomOutHeight ? zoomOutHeight : zoomOutWidth;
+
+    return InkWell(
+      child: zoomOutHeight <= 60
+          ? Container(
+              width: buttonSqueeze.value,
+              height: 60,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(247, 64, 106, 1),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: _buildInside(context),
+            )
+          : Container(
+              width: zoomSide,
+              height: zoomSide,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(247, 64, 106, 1),
+                borderRadius: BorderRadius.circular(MediaQuery.of(context).size.height - MediaQuery.of(context).size.height*buttonZoomOut.value)
+              ),
+            ),
+      onTap: () {
+        controller.forward();
+      },
+    );
+  }
+
+  Widget _buildInside(BuildContext context) {
+    if (buttonSqueeze.value > 75) {
+      return Text(
+        'Sign in',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      );
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.white),
+        strokeWidth: 1,
+      );
+    }
+  }
+}
